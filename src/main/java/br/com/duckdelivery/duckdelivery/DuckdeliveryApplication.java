aa5438954b3cf9edd40496c1a74cccb2ce16b536
VMarkz <vitor.portela@bandtec.com.br>
2021-02-24 18:08:25 -0300

    add base project generated by spring initializer

---
+++ src/main/java/br/com/duckdelivery/duckdelivery/DuckdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.duckdelivery.duckdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckdeliveryApplication.class, args);
+	}
+
+}
